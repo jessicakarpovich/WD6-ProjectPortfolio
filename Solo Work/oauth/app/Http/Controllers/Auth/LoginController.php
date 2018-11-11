5a1a4cd0b9d40ec2ad5c665988a7d68ae76e1bda
Jessica Karpovich <karpovichjm@gmail.com>
2018-11-10 21:50:21 -0600

    Added routes for Google signin

--- Solo Work/oauth/app/Http/Controllers/Auth/LoginController.php
+++ Solo Work/oauth/app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Socialite;
 
 class LoginController extends Controller
 {
@@ -35,5 +36,11 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    // redirect user to Google oAuth
+    public function redirectToProvider()
+    {
+        return Socialite::driver('google')->redirect();
     }
 }
--- Solo Work/oauth/routes/web.php
+++ Solo Work/oauth/routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// for oAuth
+Route::get('/redirect', 'Auth\LoginController@redirectToProvider');
+Route::get('/callback', 'Auth\LoginController@handleProviderCallback');
