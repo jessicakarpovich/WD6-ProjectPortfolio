93d13fd173bf4fed5852161967e755f1aeffcdb8
Jessica Karpovich <karpovichjm@gmail.com>
2018-11-16 20:36:56 -0600

    Adding update and delete

--- Solo Work/quiz3/routes/grades.js
+++ Solo Work/quiz3/routes/grades.js
@@ -6,7 +6,6 @@ const Student = require( '../models/Student' )
 // function to retrieve all student records from db
 let getStudents = ( req, res, next ) => {
     Student.find( {}, (err, students) => {
-        err
         if ( err ) {
             console.log('err', err)
         } else if ( students ) {
@@ -14,6 +13,20 @@ let getStudents = ( req, res, next ) => {
         }
         next()
     })
+}
+
+let getStudentById = ( req, res, next ) => {
+    // check it's a valid id, favico icon appears sometimes
+    if ( req.params.id.match( /^[0-9a-fA-F]{24}$/ ) ) {
+        Student.find({ _id: req.params.id }, function( err, student ) {
+            if ( err ) {
+                console.log('err', err)
+            } else if ( student ) {
+                req.student = student
+            }
+            next()
+        })
+    }
 }
 
 
@@ -70,12 +83,57 @@ router.post('/', getStudents, ( req, res, next ) => {
     }
 })
 
-router.get('/:id', (req, res, next) => {
-    
-    res.send('<h1>Hello</h1>');
-});
+router.get('/:id', getStudentById, (req, res, next) => {
+    res.render('edit', { student: req.student[0] })
+})
 
-router.delete('/:id', ( req, res, next ) => {
+router.put('/:id', getStudentById, (req, res, next) => {
+    // if there is a body being sent
+    if ( req.body ) {
+        // create a new student object
+        // don't add letter grade until it passes validation
+        let newStudent = req.student[0]
+        newStudent.name = req.body.name
+        newStudent.studentPercent = req.body.percent
+
+        // validate input for new grade
+        newStudent.validate( (err) => {
+            // show errors by rendering the page again
+            if ( err ) {
+                res.render('edit', { 
+                    student: newStudent,
+                    errors: err
+                })
+            } else {
+                const percent = req.body.percent
+                if ( percent > 89 ) {
+                    newStudent.studentLetterGrade = "A"
+                } else if ( percent > 79 ) {
+                    newStudent.studentLetterGrade = "B"
+                } else if ( percent > 69 ) {
+                    newStudent.studentLetterGrade = "C"
+                } else if ( percent > 59 ) {
+                    newStudent.studentLetterGrade = "D"
+                } else {
+                    newStudent.studentLetterGrade = "F"
+                }
+                console.log(newStudent)
+                Student.findOneAndUpdate({_id: newStudent._id}, newStudent, function(err, student) {
+                    if (err) {
+                        console.log('err', err);
+                    } else {
+                        console.log("success updating")
+                    }
+                    next()
+                })
+                // redirect back to main page
+                res.json({ status: "Success", redirect: '/' })
+            }
+        })
+    }
+})
+
+router.delete('/delete/:id', ( req, res, next ) => {
     Student.findOneAndRemove({ _id: req.params.id }, ( err, student ) => {
         if ( err ) {
             console.log( 'err', err )
